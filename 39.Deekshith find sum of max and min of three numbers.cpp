bf4a88cc91f82666e479c0bc3a7880cd76b1877b
Deekshith_5113 <94819901+Deekshith5113@users.noreply.github.com>
2021-12-08 10:31:38 +0530

    Add files via upload

---
+++ 39.Deekshith find sum of max and min of three numbers.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,min,max,sum;
+	printf("Enter the three numbers:");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	if(a>b&&a>c)
+	{
+		max=a;
+		if(b>c)
+		{
+			min=c;
+		}
+		else
+		{
+			min=b;
+		}	
+	}
+	else if(b>a&&b>c)
+	{
+		max=b;
+		if(c>a)
+		{
+			min=a;
+		}
+		else
+		{
+			min=c;
+		}	
+	}
+    else if(c>b&&c>a)
+	{
+		max=c;
+		if(b>a)
+		{
+			min=a;
+		}
+		else
+		{
+			min=b;
+		}	
+	}
+	sum=(min+max);
+	printf("The sum of min and max is %d",sum);			
+}
+
